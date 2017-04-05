91e191063e26e16422d38e9ee09f7c9068628631
Eero Paavola <paavola.ee@gmail.com>
2017-04-05 20:21:09 +0300

    Game map view. NOTE: you need to add API key to google_maps_api.xml

--- app/src/main/java/fi/semiproot/featofspeed/JoinGameActivity.java
+++ app/src/main/java/fi/semiproot/featofspeed/JoinGameActivity.java
@@ -1,7 +1,10 @@
 package fi.semiproot.featofspeed;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JoinGameActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class JoinGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_game);
+
+        Button launchButton = (Button) findViewById(R.id.launchGameButton);
+        launchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JoinGameActivity.this, GameMapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
