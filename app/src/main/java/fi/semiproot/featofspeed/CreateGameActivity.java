9007fcbc435c37183d45aa6fdf57dba00576c7f0
Lari-Matias Orjala <lorjala@student.oulu.fi>
2017-04-08 00:20:04 +0300

    Updated main menu and create game views + dpi support for drawables.

--- app/src/main/java/fi/semiproot/featofspeed/CreateGameActivity.java
+++ app/src/main/java/fi/semiproot/featofspeed/CreateGameActivity.java
@@ -1,37 +1,86 @@
 package fi.semiproot.featofspeed;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class CreateGameActivity extends AppCompatActivity {
+    RadioButton smallButton, mediumButton, largeButton;
+    TextView description;
+
+    static final String[] GAME_DESCRIPTIONS = {
+            "Maximum of 5 waypoints in 250m radius.",
+            "Maximum of 6 waypoints in 1km radius.",
+            "Maximum of 6 waypoints in 2,5km radius."
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_game);
 
-        Spinner gameSizeSpinner = (Spinner)findViewById(R.id.gameSizeSpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.game_sizes, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        gameSizeSpinner.setAdapter(adapter);
-
+        // Setup create game button:
         Button createButton = (Button)findViewById(R.id.createButton);
         createButton.setOnClickListener(new CreateButtonOnClickListener());
+
+        // Setup custom font:
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/unispace bold.ttf");
+        TextView titleCreateGame = (TextView)findViewById(R.id.title_createGame);
+        titleCreateGame.setTypeface(font);
+
+        // Setup radio buttons:
+        smallButton = (RadioButton)findViewById(R.id.radioButtonSmall);
+        mediumButton = (RadioButton)findViewById(R.id.radioButtonMedium);
+        largeButton = (RadioButton)findViewById(R.id.radioButtonLarge);
+
+        RadioButtonOnClickListener rbListener = new RadioButtonOnClickListener();
+        smallButton.setOnClickListener(rbListener);
+        mediumButton.setOnClickListener(rbListener);
+        largeButton.setOnClickListener(rbListener);
+
+        // Setup game description text:
+        description = (TextView)findViewById(R.id.descriptionText);
+        description.setText(GAME_DESCRIPTIONS[1]);
     }
 
     private class CreateButtonOnClickListener implements View.OnClickListener {
 
         @Override
         public void onClick(View view) {
-            Intent intent = new Intent(CreateGameActivity.this, LobbyActivity.class);
+            Intent intent = new Intent(CreateGameActivity.this, LoadActivity.class);
             startActivity(intent);
             CreateGameActivity.this.finish();
+        }
+    }
+
+    private class RadioButtonOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            boolean checked = ((RadioButton)v).isChecked();
+
+            switch (v.getId()) {
+                case R.id.radioButtonSmall:
+                    if (checked) {
+                        description.setText(GAME_DESCRIPTIONS[0]);
+                    }
+                    break;
+                case R.id.radioButtonMedium:
+                    if (checked) {
+                        description.setText(GAME_DESCRIPTIONS[1]);
+                    }
+                    break;
+                case R.id.radioButtonLarge:
+                    if (checked) {
+                        description.setText(GAME_DESCRIPTIONS[2]);
+                    }
+                    break;
+            }
         }
     }
 }
