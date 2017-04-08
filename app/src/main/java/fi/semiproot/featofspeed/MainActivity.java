0b174ec659254ed844da37a304ab9c48f2214a26
Eero Paavola <paavola.ee@gmail.com>
2017-04-08 14:34:35 +0300

    Anonymous authentication /w firebase.

--- app/src/main/java/fi/semiproot/featofspeed/MainActivity.java
+++ app/src/main/java/fi/semiproot/featofspeed/MainActivity.java
@@ -7,11 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -20,26 +28,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        final TextView loginStatusLabel = (TextView) findViewById(R.id.loginStatusLabel);
+
         Button createGameButton = (Button)findViewById(R.id.createGameButton);
         createGameButton.setOnClickListener(new CreateGameButtonListener());
 
         Button joinGameButton = (Button)findViewById(R.id.joinGameButton);
         joinGameButton.setOnClickListener(new JoinGameButtonListener());
 
-        /*
+
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    Log.d("FeatOfSpeed", "onAuthStateChanged:signed_in:" + user.getUid());
+                    String displayName = user.getDisplayName();
+                    if (displayName == null) {
+                        displayName = "Guest_" + user.getUid().substring(0, 5);
+                    }
+                    String userUid = user.getUid();
+                    Log.d(TAG, "onAuthStateChanged:signed_in: " + userUid);
+                    loginStatusLabel.setText(getString(R.string.signed_in_name_label, displayName));
+                    Log.d(TAG, "onAuthStateChanged:signed_in displayName" + displayName);
                 } else {
-                    Log.d("FeatOfSpeed", "onAuthStateChanged:signed_out");
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    loginStatusLabel.setText(getString(R.string.signed_out_name_label));
                 }
             }
         };
-        */
+
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInAnonymously:onComplete: " + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInAnonymously", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication successful.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     private class CreateGameButtonListener implements View.OnClickListener {
