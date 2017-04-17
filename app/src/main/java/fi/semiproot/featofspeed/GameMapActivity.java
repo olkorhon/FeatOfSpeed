f961bbd8550e11c2ab8f0bd6d7acf63a6dd8a33d
Eero Paavola <paavola.ee@gmail.com>
2017-04-17 18:16:11 +0300

    Initial location services implementation.

--- app/src/main/java/fi/semiproot/featofspeed/GameMapActivity.java
+++ app/src/main/java/fi/semiproot/featofspeed/GameMapActivity.java
@@ -1,15 +1,36 @@
 package fi.semiproot.featofspeed;
 
 import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Handler;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,10 +38,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GameMapActivity extends FragmentActivity implements OnMapReadyCallback, SensorEventListener {
+public class GameMapActivity extends FragmentActivity implements OnMapReadyCallback, SensorEventListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final String TAG = GameMapActivity.class.getSimpleName();
+
+    private static final int REQUEST_CHECK_SETTINGS = 9999;
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 8888;
+    private static final String REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key";
+    private static final String LOCATION_KEY = "location-key";
 
     private GoogleMap mMap;
-
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    private boolean mRequestingLocationUpdates = false;
 
     // Compass functionality
     CompassView compassWidget;
@@ -52,19 +83,70 @@ public class GameMapActivity extends FragmentActivity implements OnMapReadyCallb
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_map);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        updateValuesFromBundle(savedInstanceState);
+
+        // Make sure that location services provides the needed accuracy
+        mLocationRequest = createLocationRequest(10000, 5000);
+
+        LocationSettingsRequest.Builder settingsBuilder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, settingsBuilder.build());
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates locationSettingsStates = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can
+                        // initialize location requests here.
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed
+                        // by showing the user a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(
+                                    GameMapActivity.this,
+                                    REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way
+                        // to fix the settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-
         // Fetch relevant sensor instances
-        sManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelometer = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetometer = sManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
         // Fetch compass view instance from content
-        compassWidget = (CompassView)findViewById(R.id.compass);
+        compassWidget = (CompassView) findViewById(R.id.compass);
 
         rotationMatrix = new float[9];
         orientation = new float[3];
@@ -72,35 +154,74 @@ public class GameMapActivity extends FragmentActivity implements OnMapReadyCallb
         compassUpdateHandler = new Handler();
     }
 
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+                mRequestingLocationUpdates = savedInstanceState.getBoolean(REQUESTING_LOCATION_UPDATES_KEY);
+            }
+            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+            }
+        }
+    }
 
+    protected LocationRequest createLocationRequest(int interval, int fastestInterval) {
+        Log.d(TAG, "createLocationRequest() was called");
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(interval);
+        locationRequest.setFastestInterval(fastestInterval);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    private void askLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart() was called");
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume() was called");
+        super.onResume();
+        Log.d(TAG, "," + mRequestingLocationUpdates);
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
         // Start refreshing compass
         compassUpdating = true;
         compassUpdateHandler.post(compassUpdaterRunnable);
 
-        if (accelometer == null)
-        {
+        if (accelometer == null) {
             Toast.makeText(this, "Accelometer not found!", Toast.LENGTH_SHORT).show();
-        }
-        else if (magnetometer == null)
-        {
+        } else if (magnetometer == null) {
             Toast.makeText(this, "Magnetometer not found!", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
+        } else {
             sManager.registerListener(this, accelometer, 60000);
             sManager.registerListener(this, magnetometer, 60000);
         }
     }
 
     @Override
-    protected void onStop()
-    {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
 
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
         // Stop updating compass
         compassUpdating = false;
         sManager.unregisterListener(this);
@@ -117,19 +238,18 @@ public class GameMapActivity extends FragmentActivity implements OnMapReadyCallb
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady() was called");
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         float zoomLevel = 10.0f; //This goes up to 21
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel));
     }
 
 
     @Override
-    public void onSensorChanged(SensorEvent event)
-    {
+    public void onSensorChanged(SensorEvent event) {
         if (event.sensor == accelometer) {
             lastAcceleration = event.values;
             //Log.d("Acceleration", event.values.toString());
@@ -152,16 +272,86 @@ public class GameMapActivity extends FragmentActivity implements OnMapReadyCallb
 
             if (compassWidget != null) {
 
-                float angle = orientation[0] * (180.0f / (float)Math.PI);
+                float angle = orientation[0] * (180.0f / (float) Math.PI);
                 //Log.d("Orientation", Float.toString(angle) + ":" + Float.toString(orientation[0]));
                 compassWidget.setActualRotation(angle);
             }
         }
     }
 
+    // GoogleApiClient.ConnectionCallbacks
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy)
-    {
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, "onAccuracyChanged() was called");
+    }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected() was called");
+        askLocationPermission();
+        if (!mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        Log.d(TAG, "startLocationUpdates() was called");
+        mRequestingLocationUpdates = true;
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        Log.d(TAG, "stopLocationUpdates() was called");
+        mRequestingLocationUpdates = false;
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended() was called");
+    }
+    // END GoogleApiClient.ConnectionCallbacks
+
+    // GoogleApiClient.OnConnectionFailedListener
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed() was called");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // Callback to handle askLocationPermission()
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+                    mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                    if (mCurrentLocation != null) {
+                        Log.d(TAG, "Latitude: " + mCurrentLocation.getLatitude());
+                        Log.d(TAG, "Longitude: " + mCurrentLocation.getLongitude());
+                    } else {
+                        Log.d(TAG, "mCurrentLocation was null!");
+                    }
+
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged() was called");
+        mCurrentLocation = location;
+        // UpdateUI();
+        Log.d(TAG, "onLocationChanged: " + location.getLatitude() + "; " + location.getLongitude());
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY, mRequestingLocationUpdates);
+        savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
