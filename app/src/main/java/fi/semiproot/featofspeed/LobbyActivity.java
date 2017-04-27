5b37847e17544dd8b172d6e789a75c47bf9e2425
Olli Korhonen <olkorhon@gmail.com>
2017-04-27 19:16:13 +0300

    Lobby can now react to changing player status

--- app/src/main/java/fi/semiproot/featofspeed/LobbyActivity.java
+++ app/src/main/java/fi/semiproot/featofspeed/LobbyActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,14 +17,24 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.lang.Thread.sleep;
 
 public class LobbyActivity extends AppCompatActivity {
+    private static String TAG = "FOS_LobbyActivity";
 
     private boolean isHost;
     private String code;
@@ -37,6 +48,9 @@ public class LobbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        // Initiialize firebase connection
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
 
         // Get extras:
         Intent intent = getIntent();
@@ -80,6 +94,48 @@ public class LobbyActivity extends AppCompatActivity {
 
         pAdapter = new PlayerAdapter();
         playerListView.setAdapter(pAdapter);
+
+
+        if (code != null) {
+            // Link to database updates that happen to this game
+            DatabaseReference myRef = database.getReference("games/" + code);
+            myRef.addValueEventListener(new ValueEventListener() { // Read from the database
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    String value = dataSnapshot.toString();
+                    Log.d(TAG, "Value is: " + value);
+
+                    // Fetch players
+                    List<String> player_list = (List<String>) (dataSnapshot.child("players").getValue());
+                    List<Object> player_history_list = (List<Object>) (dataSnapshot.child("player_history").getValue());
+
+                    // Clear old players and add new ones
+                    players.clear();
+                    for (int i = 0; i < player_history_list.size(); i++) {
+                        // We know what is in the list so we can cast the player_history_list element to a Map<String, Object>
+                        Map<String, Object> player_history_map = (Map<String, Object>) player_history_list.get(i);
+
+                        // Try to find a matching id from the list of current players
+                        for (String player_id : player_list) {
+                            // If the player is in the game, add to list
+                            if (player_id.equals(player_history_map.get("user_id"))) {
+                                players.add(new Player(player_id, (String)player_history_map.get("nickname")));
+                            }
+                        }
+                    }
+
+                    pAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+        }
     }
 
     private class PlayerAdapter extends BaseAdapter {
